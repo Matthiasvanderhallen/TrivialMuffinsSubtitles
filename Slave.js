2352b7fad2fef8d651ca10fec3f2821c60aa60be
Matthias van der Hallen <matthias.vanderhallen@gmail.com>
2015-09-26 00:04:27 +0200

    Reconnecting Clients (1)

---
+++ Slave.js
@@ -0,0 +1,145 @@
+var inject = require('reconnect-core');
+//var WebSocket = require('ws');
+
+window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+window.WebSocket.prototype.on = function (event, callback) {
+  this['on'+event] = callback;
+  return this;
+};
+
+window.WebSocket.prototype.once = function (event, callback) {
+	alert('t2');
+    var self = this;
+  this['on'+event] = function () {
+    callback.apply(callback, arguments);
+    self['on'+event] = null;
+  };
+  return this;
+};
+
+
+window.WebSocket.prototype.off = function (event, callback) {
+	alert('t3');
+  this['on'+event] = callback;
+  return this;
+};
+
+window.WebSocket.prototype.removeListener = function (event, callback) {
+	this['on'+event] = null;
+}
+
+var connection = new WebSocket('ws://localhost:1337');
+console.log(connection);
+
+//var connection = new WebSocket('ws://localhost:1337');
+//console.log(connection);
+
+//console.log(WebSocket);
+
+//var client;// = new WebSocket();
+
+var nederlandsTitel = d3.select("#Nederlands").selectAll("h1");
+var fransTitel = d3.select("#Frans").selectAll("h1");
+
+var id;
+
+var subtitleInfo = {nl: "", fr: "", size: 72, mode: "dual"};
+
+//connection.onmessage = onMessage;
+
+function onMessage(message){
+	var json;
+
+	console.log('received: ' + message.data);
+
+	try{
+		json = JSON.parse(message.data);
+	}catch (e) {
+		console.log(message.data);
+    	return;
+	}
+
+	if(json.type == 'id'){
+		id = json.id;
+	}
+
+	if(json.type == 'subtitle'){
+		subtitleInfo.nl = json.nl;
+		subtitleInfo.fr = json.fr;
+		showSubtitle(json.nl, json.fr);
+	}
+	
+	if(json.type == 'size'){
+		subtitleInfo.size = json.size;
+		console.log('I resize: ' + json.size);
+		setSize(json.size);
+	}
+
+	if(json.type == 'mode'){
+		console.log(json.peer + " == " + id + "?");
+		if(json.peer == id){
+			subtitleInfo.mode = json.mode;
+			setVisibility(json.mode);
+		}
+	}
+
+	if(json.type == 'identify'){
+		identify();
+	}
+};
+
+function identify(){
+	setVisibility("dual");
+	showSubtitle(id,id);
+	setTimeout(showSubtitle, 500, subtitleInfo.nl, subtitleInfo.fr);
+	setTimeout(setVisibility, 500, subtitleInfo.mode);
+};
+
+function setSize(size){
+	nederlandsTitel.style("font-size", size + "px");
+	fransTitel.style("font-size", size + "px");
+}
+
+function showSubtitle(nl, fr) {
+	nederlands = nederlandsTitel.data([nl]);
+	frans = fransTitel.data([fr]);
+
+	var returnText = function (d){
+		return d;
+	}
+
+	nederlands.text(returnText);
+	frans.text(returnText);
+};
+
+function setVisibility(mode){
+	if(mode == "nl"){
+		d3.select("#separator").style("visibility", "collapse");
+		d3.select("#Frans").style("visibility", "collapse");
+	}
+	if(mode == "fr"){
+		d3.select("#separator").style("visibility", "collapse");
+		d3.select("#Nederlands").style("visibility", "collapse");
+	}
+	if(mode == "dual"){
+		d3.select("#separator").style("visibility", "visible");
+		d3.select("#Nederlands").style("visibility", "visible");
+		d3.select("#Frans").style("visibility", "visible");
+	}
+};
+
+var reconnect = inject(function(){
+	var connection = new WebSocket('ws://localhost:1337');
+	console.log(connection);
+	connection.onmessage = onMessage;
+	return connection;
+});
+
+var re = reconnect({}, function (stream) {
+  // stream = the stream you should consume 
+})
+.on('connect', function (con) {
+	console.log(con);
+})
+.connect();
